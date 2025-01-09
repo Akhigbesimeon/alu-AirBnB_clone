d4332695e584bd8b09bf0388846da9be6ad82e03
WagnerMushayija <mushayijawagner@gmail.com>
2025-01-09 16:28:30 +0200

    added test file for city correctly

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+# """ City Module for HBNB project """
+# from models.base_model import BaseModel
+#
+#
+# class City(BaseModel):
+#     """ The city class, contains state ID and name """
+#     state_id = ""
+#     name = ""
 
-
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+# Define the function to test
+def multiply(a, b):
+    return a * b
