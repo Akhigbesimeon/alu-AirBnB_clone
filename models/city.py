e4092cf1017ea086db213538bf8372ef8a545318
WagnerMushayija <mushayijawagner@gmail.com>
2025-01-09 16:45:00 +0200

    added test file for city cor

--- models/city.py
+++ models/city.py
@@ -1,31 +1,30 @@
-<<<<<<< HEAD
-#!/usr/bin/python
-""" holds class City"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+# <<<<<<< HEAD
+# #!/usr/bin/python
+# """ holds class City"""
+# import models
+# from models.base_model import BaseModel, Base
+# from os import getenv
+# import sqlalchemy
+# from sqlalchemy import Column, String, ForeignKey
+# from sqlalchemy.orm import relationship
+#
+#
+# class City(BaseModel, Base):
+#     """Representation of city """
+#     if models.storage_t == "db":
+#         __tablename__ = 'cities'
+#         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+#         name = Column(String(128), nullable=False)
+#         places = relationship("Place", backref="cities")
+#     else:
+#         state_id = ""
+#         name = ""
+#
+#     def __init__(self, *args, **kwargs):
+#         """initializes city"""
+#         super().__init__(*args, **kwargs)
 
-
-class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        state_id = ""
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
-=======
 #!/usr/bin/python3
 # Define the function to test
 def multiply(a, b):
     return a * b
->>>>>>> d4332695e584bd8b09bf0388846da9be6ad82e03
