09f1310fb81e285b91d3405e011bdc29f27f7cd5
WagnerMushayija <mushayijawagner@gmail.com>
2025-01-09 15:30:08 +0200

    added test file for city

--- tests/test_models/test_engine/test_city.py
+++ tests/test_models/test_engine/test_city.py
@@ -1,26 +1,11 @@
+# Create the test case for city
 import unittest
-from city import City
+from models.city import multiply
+class TestMult(unittest.TestCase):
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 4), 8)  # 2 * 4 = 8
+        self.assertEqual(multiply(0, 3), 0)  # 0 * 3 = 0
 
-
-class TestCity(unittest.TestCase):
-
-    def test_city_creation(self):
-        # Test creation of an Amenity object
-        amenity = City("Free WiFi", "High-speed internet access")
-        self.assertEqual(amenity.name, "Free WiFi")
-        self.assertEqual(amenity.description, "High-speed internet access")
-
-    def test_display_info(self):
-        # Test the display_info method
-        city = City("Free WiFi", "High-speed internet access")
-        self.assertEqual(city.display_info(), "Amenity: Free WiFi, Description: High-speed internet access")
-
-    def test_update_description(self):
-        # Test the update_description method
-        city = City("Free WiFi", "High-speed internet access")
-        city.update_description("Faster internet connection")
-        self.assertEqual(city.description, "Faster internet connection")
-
-
-if __name__ == '__main__':
+# Run the tests when the file is executed
+if __name__ == "__main__":
     unittest.main()
