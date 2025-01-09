d228cc1a1723102b58c8524efb1da529afc9a8d8
WagnerMushayija <mushayijawagner@gmail.com>
2025-01-09 20:08:07 +0200

    after praying, it worked Everythingggg

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import unittest
+from models.base_model import BaseModel
+class TestBaseModel(unittest.TestCase):
+    def test_base_model(self):
+        """Set up method that will run before every Test"""
+        pass
