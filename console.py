bde76feabd24e412444af40f742ec396a4005c48
WagnerMushayija <mushayijawagner@gmail.com>
2025-01-12 14:17:41 +0200

    did the console with documentation

--- console.py
+++ console.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import cmd
+
 """
 Entry point of the command interpreter for the HBNB project.
 This module uses the cmd module to create an interactive shell.
 """
+import cmd
+
 class HBNBCommand(cmd.Cmd):
     """
     Command interpreter for the HBNB project.
