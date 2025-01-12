8017b7142dfb8bd25d397dfa37ee0d0f28ecbc92
WagnerMushayija <mushayijawagner@gmail.com>
2025-01-12 13:35:29 +0200

    tried base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,23 @@
-import uuid
+"""docstring for the whole module"""
+
 from datetime import datetime
+import uuid
+
 
 class BaseModel:
-    """Base class for all models"""
-    def __init__(self, *args, **kwargs):
-      pass
+    """BaseModel class"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now() 
+    def __str__(self):
+        return f"[{self.__class__}] ({self.id}) {self.__dict__}"
+    def save(self):
+        self.updated_at = datetime.now()  
+    def to_dict(self):
+        iso_time = "%Y-%m-%d %H:%M:%S.%f"
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.strftime(iso_time)
+        rdict["updated_at"] = self.updated_at.strftime(iso_time)
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
