3e2ff5b806ede55f491f4677fd1e6c467274bb53
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-01-12 10:49:38 +0200

    pushing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,22 +22,19 @@ class FileStorage:
         self.__objects[key] = obj
 
     def save(self):
-    """ Serializes __objects to the JSON file."""
-    obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-    with open(self.__file_path, "w") as f:
-        json.dump(obj_dict, f)
-
-    def reload(self):
-    """Deserializes the JSON file to __objects, only if the file exists."""
+        """Serializes __objects to the JSON file."""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+    
+   def reload(self):
+    """Deserializes the JSON file to __objects, if it exists."""
     try:
         with open(self.__file_path, "r") as f:
             obj_dict = json.load(f)
-        # Recreate instances and store them in __objects
         for key, value in obj_dict.items():
             class_name = value["__class__"]
-            # Dynamically get the class (ensure all classes are imported in models.__init__.py)
-            obj_class = globals().get(class_name)
-            if obj_class:
-                self.__objects[key] = obj_class(**value)
+            self.__objects[key] = globals()[class_name](**value)
     except FileNotFoundError:
         pass
+
