7ef52fce454daee426075c6d7056aa77cd527c1f
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-01-12 10:33:02 +0200

    pushing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,46 @@
-#!/usr/bin/python
-import sqlite3
+import json
 from models.base_model import BaseModel
 
 class FileStorage:
-    """FileStorage class"""
-    __file_path = "file.db"
-    __objects = {}
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances.
+    """
+    __file_path = "file.json"  # Path to the JSON file
+    __objects = {}  # Dictionary to store objects
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file.
+        """
+        # Create a dictionary with dictionary representations of all objects
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects, only if the file exists.
+        """
+        try:
+            with open(self.__file_path, "r") as f:
+                obj_dict = json.load(f)
+            # Recreate instances and store them in __objects
+            for key, value in obj_dict.items():
+                class_name = value["__class__"]
+                if class_name == "BaseModel":
+                    self.__objects[key] = BaseModel(**value)
+        except FileNotFoundError:
+            pass
