113c3daf83ddf6ddce3f183e352042c33e69e357
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-01-12 10:58:31 +0200

    pushing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,39 +2,28 @@ import json
 from models.base_model import BaseModel
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file and deserializes JSON file to instances.
-    """
-    __file_path = "file.json"  # Path to the JSON file
-    __objects = {}  # Dictionary to store objects
+    __file_path = 'file.json'
+    __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary __objects.
-        """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id.
-        """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file."""
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, "w") as f:
-            json.dump(obj_dict, f)
-    
-   def reload(self):
-    """Deserializes the JSON file to __objects, if it exists."""
-    try:
-        with open(self.__file_path, "r") as f:
-            obj_dict = json.load(f)
-        for key, value in obj_dict.items():
-            class_name = value["__class__"]
-            self.__objects[key] = globals()[class_name](**value)
-    except FileNotFoundError:
-        pass
+        import json
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(self.__objects, f)
 
+    def reload(self):
+        """Reloads the objects from the JSON file"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)
+        except FileNotFoundError:
+            pass
+        except json.JSONDecodeError:
+            pass
