6c05beef55dbfdc846435e7ba68c4fb81739c2d5
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-01-12 10:53:36 +0200

    pushing

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-from models.review import Review
 import unittest
+from models.review import Review
+
 class TestReview(unittest.TestCase):
     def test_review(self):
         """Set up method that will run before every Test"""
