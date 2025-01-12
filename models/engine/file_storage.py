a73eb4d32e05e24726570c94658193ac214fb712
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-01-12 11:17:47 +0200

    pushing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,19 +34,15 @@ class FileStorage:
             json.dump(dict_representation, f)
 
     def reload(self):
-    """
-    Deserializes objects from the JSON file to storage
-    """
-    try:
-        with open(FileStorage.__file_path, 'r') as f:
-            dict_representation = json.load(f)
-            for key, value in dict_representation.items():
-                class_name = value["__class__"]
-                class_obj = globals()[class_name]  # Get the class by name
-                self.new(class_obj(**value))
-    except FileNotFoundError:
-        pass  # If file does not exist, we just skip loading
-    except json.JSONDecodeError:
-        print("Warning: file.json is empty or corrupted, starting with an empty storage.")
-        # Optionally create an empty file or handle this scenario
-        pass
+        """
+        Deserializes objects from the JSON file to storage.
+        """
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                dict_representation = json.load(f)
+                for key, value in dict_representation.items():
+                    class_name = value["__class__"]
+                    class_obj = globals()[class_name]  # Get the class by name
+                    self.new(class_obj(**value))
+        except FileNotFoundError:
+            pass
